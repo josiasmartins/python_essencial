8419ca0688dc723f0748b408c8831865c5d7d02a
josiasmartins <josiasmartinscaitano@gmail.com>
2025-02-02 09:57:44 -0300

    feature: create file for exercises in var and type data in python

---
+++ s3-exercicios/exercicio1.py
@@ -0,0 +1,7 @@
+"""
+2. Faça um programa que leia um número inteiro e imprima-o
+"""
+
+numero: int = int(input("Informe um número inteiro: "))
+
+print(numero)
---
+++ s3-exercicios/exercicio2.py
@@ -0,0 +1,12 @@
+"""
+2. Faça um programa que peça para o usuário digitar três valores inteiros e imprima a soma deles.
+"""
+
+numero1: int = int(input("Digite o primeiro valor: "))
+numero2: int = int(input("Digite o segundo valor: "))
+numero3: int = int(input("Digite o terceiro valor: "))
+
+soma: int = numero1 + numero2 + numero3
+
+print(f"A soma dos números {numero1} com {numero2} e {numero3} é {soma}")
+
